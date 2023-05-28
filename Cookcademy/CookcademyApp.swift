c3cc3b97c216bfc076fae6f1419685499997fba1
rfarwell <roryfarwell@outlook.com>
2023-05-28 18:24:11 +0100

    Created Cookcademy Project files

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Rory Farwell on 28/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
